fed66cb8887387bc2f3b746631c730d56f6048c1
Aleks <bla3e316@gmail.com>
2017-11-02 15:02:09 +0200

    add css support to conf webpack*

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,5 +1,8 @@
+import webpack from 'webpack';
 import Config from 'webpack-config';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import autoprefixer from 'autoprefixer';
+import precss from 'precss';
 
 export default new Config().merge({
     entry: './src/index.js',
@@ -19,5 +22,7 @@ export default new Config().merge({
         new HtmlWebpackPlugin({
             template: './src/index.html',
             inject: "body"
-        })]
+        }),
+        new webpack.LoaderOptionsPlugin({ options: { postcss: [precss, autoprefixer] } })
+    ]
 });
--- config/webpack.production.config.js
+++ config/webpack.production.config.js
@@ -5,6 +5,25 @@ export default new Config().extend('conf/webpack.base.config.js').merge({
     output: {
         filename: 'bundle.min.js'
     },
+    devtool: 'source-map',
+    module: {
+        loaders: [{
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                {
+                    loader: 'css-loader',
+                    options: {
+                        modules: true,
+                        importLoaders: 1,
+                        localIdentName: "[hash:base64:10]",
+                        minimize: true
+                    }
+                },
+                { loader: 'postcss-loader' },
+            ]
+        }]
+    },
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
             compress: {
